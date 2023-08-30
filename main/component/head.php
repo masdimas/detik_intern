238528de4ea161fc9d5b8eebae13f34c3e580239
Arraidh <ari.darmika@gmail.com>
2023-08-30 17:20:13 +0700

    fixing font cdn

--- main/component/head.php
+++ main/component/head.php
@@ -19,8 +19,7 @@
     <link rel="stylesheet" href="./assets/css/about-swiper.css">
     <link rel="stylesheet" href="./assets/css/footer.css">
     
-    <link rel="stylesheet" href="./assets/include/swiper-min/swiper.min.css">
-  
+<link rel="stylesheet" href="./assets/include/swiper/swiper.min.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -28,6 +27,12 @@
       href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap"
       rel="stylesheet"
     />
-    
-    <link rel="stylesheet" href="./assets/include/bootstrap/css/bootstrap.min.css">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
+    />
+    <!-- <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+    /> -->
   </head>
