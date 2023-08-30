93ca09045b36d971c3071e1ca2f9a3fc04f78222
Arraidh <ari.darmika@gmail.com>
2023-08-30 12:53:22 +0700

    Swipper Connect

--- main/component/foot.php
+++ main/component/foot.php
@@ -1,6 +1,6 @@
     <!-- <script src="./js/navbar.js"></script> -->
     <script src="./assets/js/navbar.js"></script>
-    <script src="./assets/js/script.js" type="module"></script>
+    <script src="./assets/js/swiper.js" type="module"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
     <script
       src="https://kit.fontawesome.com/f5fe63c5d0.js"
