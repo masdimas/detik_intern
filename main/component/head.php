c0f28ff86a05fab1affe93d9102f2e581a5df288
Arraidh <ari.darmika@gmail.com>
2023-09-01 13:15:15 +0700

    Connecting Bootstrap Via local file

--- main/component/head.php
+++ main/component/head.php
@@ -16,21 +16,8 @@
     
     <!-- Style Sheet -->
     <!-- Library -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-      crossorigin="anonymous"
-      />
+    <link rel="stylesheet" href="./assets/include/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/include/swiper/swiper.min.css">
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
-    />
 
     <!-- CSS -->
     <link rel="stylesheet" href="./assets/css/general.css">
@@ -39,12 +26,5 @@
     <link rel="stylesheet" href="./assets/css/about-video.css">
     <link rel="stylesheet" href="./assets/css/about-swiper.css">
     <link rel="stylesheet" href="./assets/css/footer.css">
-    
-    <!-- Font -->
-    <!-- <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap"
-      rel="stylesheet"
-    /> -->
+  
   </head>
