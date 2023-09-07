4c56d6574e98eac28fc9e526cf5749c6270c8900
rezzaazaa <arez.atlantiz@gmail.com>
2023-09-07 21:45:56 +0700

    Update form-modal.php

--- festivalcreative-lps/section/home/form-modal.php
+++ festivalcreative-lps/section/home/form-modal.php
@@ -7,7 +7,7 @@
                 <!-- Modal Header -->
                 <div class="modal-header">
 
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"><img src="./src/image/form/close.png" alt=""></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"><img src="./src/image/home/form/close.png" alt=""></button>
                 </div>
 
                 <!-- Modal body -->
