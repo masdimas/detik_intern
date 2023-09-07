356e8f8d98de02865c6caefd12fe9e3a039921cb
rezzaazaa <arez.atlantiz@gmail.com>
2023-09-07 12:11:13 +0700

    fest lps update 02

---
+++ festivalcreative-lps/snk-view.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<?php include "component/head.php"; ?>
+
+<body>
+    <?php include "section/home/navbar.php"; ?>
+    <?php include "component/snk.php"; ?>
+    <?php include "section/home/footer.php"; ?>
+</body>
+<?php include "component/foot.php"; ?>
+
+</html>
