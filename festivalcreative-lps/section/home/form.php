b99cee57ab0a940cb6aa0a4d3b3d76e93fce7972
Dimas <dimas.septiandi@detik.com>
2023-09-11 17:02:10 +0700

    Update Desc LPS

--- festivalcreative-lps/section/home/form.php
+++ festivalcreative-lps/section/home/form.php
@@ -8,13 +8,17 @@
                             <img class="about-icon" src="./src/image/home/form/about-icon.png" alt="about-icon">
                         </h1>
                         <div class="title text-center m-2">
-                            <span class="">CREARTIVE LPS 2023</span>
+                            <span class="">TENTANG FESTIVAL CREARTIVE LPS 2023</span>
                         </div>
                         <div class="konten">
-                            <p>FESTIVAL CREARTIVE LPS 2023 adalah festival kreatif yang diselenggarakan oleh Lembaga Penjamin Simpanan (LPS). LPS merupakan lembaga Independen yang bertugas menjamin simpanan nasabah dan turut aktif dalam memelihara stabilitas perbankan sesuai kewenangannya. Dalam hal ini, LPS mengundang masyarakat Indonesia dari berbagai kalangan untuk mengungkapkan ide kreatif mereka mengenai literasi keuangan melalui kompetisi video pendek dan poster.</p>
-                            <p>Dengan hashtag <b><i>#LPSBangunGenerasiCreArtive</i></b> , LPS berkomitmen untuk menjadi wadah aman yang mendukung kreativitas positif, mengajak semua orang untuk aktif berpartisipasi dalam meningkatkan pemahaman tentang pengelolaan keuangan yang baik.</p>
+                            <p>FESTIVAL CREARTIVE LPS 2023 merupakan ajang kompetisi video pendek dan poster iklan layanan masyarakat yang ditujukan bagi insan kreatif Indonesia. Kegiatan ini secara rutin diselenggarakan oleh Lembaga Penjamin Simpanan (LPS) dan telah memasuki tahun kedua. Di tahun ini, LPS menambahkan satu kategori kompetisi yaitu kompetisi poster iklan layanan masyarakat. </p>
+                            <p>Mengusung tema <b>&quot;Bangun Budaya Menabung, Wujudkan Finansial yang Sehat&quot;</b>, LPS mengajak seluruh lapisan masyarakat untuk berkontribusi dan memberikan ide kreatif sekaligus mengedukasi masyarakat mengenai manfaat <b>menabung di bank</b>. Melalui tagline #LPSBangunGenerasiCreArtive, LPS berkomitmen untuk mendukung ide-ide kreatif dari generasi muda Indonesia untuk turut aktif berpartisipasi dalam meningkatkan pemahaman masyarakat mengenai peran dan fungsi LPS.</p>
+                            <p>
+                            Sekilas mengenai LPS:<br>
+                            LPS merupakan lembaga yang memiliki fungsi untuk menjamin simpanan nasabah di bank dan turut aktif dalam menjaga stabilitas sistem perbankan Indonesia.
+                            </p>
                             <p class="text-center cta-lps">
-                                Website: <a href="https://www.lps.go.id/" target="_blank">lps.go.id</a>
+                                Website: <a href="https://www.lps.go.id/" target="_blank"> lps.go.id</a>
                             </p>
                         </div>
                     </div>
