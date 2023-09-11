1e37ce62cb8b7d91b762e6fb8158e6bab68485b7
Arraidh <ari.darmika@gmail.com>
2023-09-11 14:56:06 +0700

    File: Adding Welcome Page Photo Gallery to PHP Component File

--- yamaha-youth/main/component/photo-gallery.php
+++ yamaha-youth/main/component/photo-gallery.php
@@ -1,8 +1,7 @@
-
-<section id="photo-gallery">
-      <div class="section-container">
+<section id="photo-gallery" class="pt-3 pb-5">
+      <div class="section-container pg-container px-4">
         <h1 class="text-center gill-sans-1000 mt-5">PHOTO GALLERY</h1>
-        <div class="gallery-swiper mySwiper2 mt-5">
+        <div class="swiper pg-swiper mySwiper2 mt-4 mt-md-5">
           <div class="swiper-wrapper">
             <div class="swiper-slide">
               <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
@@ -35,10 +34,14 @@
               <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
             </div>
           </div>
-          <div class="swiper-button-next"></div>
-          <div class="swiper-button-prev"></div>
+          <div class="swiper-button-next pg-arrow-next">
+            <img src="./assets/images/photo-gallery/pg-arrow-next.svg" alt="" />
+          </div>
+          <div class="swiper-button-prev pg-arrow-prev">
+            <img src="./assets/images/photo-gallery/pg-arrow-prev.svg" alt="" />
+          </div>
         </div>
-        <div thumbsSlider="" class="gallery-swiper mySwiper">
+        <div thumbsSlider="" class="swiper pg-swiper mySwiper">
           <div class="swiper-wrapper">
             <div class="swiper-slide">
               <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
