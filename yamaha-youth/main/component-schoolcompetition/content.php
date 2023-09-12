e1ff0eda5285f2a9819ed6bbb16a1135e76ff4ff
rezzaazaa <arez.atlantiz@gmail.com>
2023-09-12 17:01:14 +0700

    yamaha sc

--- yamaha-youth/main/component-schoolcompetition/content.php
+++ yamaha-youth/main/component-schoolcompetition/content.php
@@ -4,28 +4,36 @@
             <div class="row justify-content-center g-2">
                 <div class="col-12 col-md-4">
                     <div class="left-side">
-                        <div class="destination">
+                        <div class="destination ">
                             <div class="latarkota">
                                 <img class="img-fluid" src="./assets/images/school-competition/Background Kota.png" alt="">
-                            </div>
-                            <div class="about-destination text-center">
-                                <div class="title">
-                                    <h4>SCHOOL COMPETITION</h4>
-                                    <div class="captions">
-                                        <p>
-                                            School Competition Classy Yamaha Youth Festival kota Bandung membawa siswa-siswi SMA terpilih untuk bertanding di [Nama GOR]. Menghadirkan berbagai kompetisi menarik seperti basket, futsal, cheerleading, dan Kompetisi OOTD (Outfit of The Day).
-                                        </p>
+
+                                <div class="wrap-desc mx-auto d-block">
+                                    <div class="logo-wrap">
+                                        <img class="img-fluid mx-auto d-block" src="./assets/images/school-competition/img-school.png" alt="">
+                                    </div>
+                                    <div class="about-destination text-center">
+                                        <div class="title">
+                                            <h4>SCHOOL COMPETITION</h4>
+                                            <h4>BANDUNG</h4>
+                                            <div class="captions">
+                                                <p>
+                                                    School Competition Classy Yamaha Youth Festival kota Bandung membawa siswa-siswi SMA terpilih untuk bertanding di [Nama GOR]. Menghadirkan berbagai kompetisi menarik seperti basket, futsal, cheerleading, dan Kompetisi OOTD (Outfit of The Day).
+                                                </p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="share">
+
+                            <!-- <div class="share">
 
                             </div>
                             <div class="wrap-kategori">
                                 <div class="kategori">Best Moments</div>
                                 <div class="kategori">Miss Classy</div>
                                 <div class="kategori">Outfit Of The Day</div>
-                            </div>
+                            </div> -->
                         </div>
                     </div>
                 </div>
