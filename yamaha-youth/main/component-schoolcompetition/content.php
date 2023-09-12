0fe1ca51d5cdd979d47e70f639a6ef556b7419de
rezzaazaa <arez.atlantiz@gmail.com>
2023-09-12 14:52:42 +0700

    school

--- yamaha-youth/main/component-schoolcompetition/content.php
+++ yamaha-youth/main/component-schoolcompetition/content.php
@@ -1,21 +1,45 @@
-<section>
+<section id="school-content">
     <div class="container section-container">
-        <div id="header">
-            <h2>Header</h2>
-            <p>This is the header</p>
-        </div>
+        <div class="wrap-content">
+            <div class="row justify-content-center g-2">
+                <div class="col-12 col-md-4">
+                    <div class="left-side">
+                        <div class="destination">
+                            <div class="latarkota">
+                                <img class="img-fluid" src="./assets/images/school-competition/Background Kota.png" alt="">
+                            </div>
+                            <div class="about-destination text-center">
+                                <div class="title">
+                                    <h4>SCHOOL COMPETITION</h4>
+                                    <div class="captions">
+                                        <p>
+                                            School Competition Classy Yamaha Youth Festival kota Bandung membawa siswa-siswi SMA terpilih untuk bertanding di [Nama GOR]. Menghadirkan berbagai kompetisi menarik seperti basket, futsal, cheerleading, dan Kompetisi OOTD (Outfit of The Day).
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="share">
 
-        <div id="sidebarWrap">
-            <div id="sidebar">
-                <h2>Sidebar</h2>
-                <p>Sidebar has content in it and will be sticky until it's bottom container reaches the footer container. Then it will scroll up as expected. Oh YEAH</p>
+                            </div>
+                            <div class="wrap-kategori">
+                                <div class="kategori">Best Moments</div>
+                                <div class="kategori">Miss Classy</div>
+                                <div class="kategori">Outfit Of The Day</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-8">
+                    <div class="content">
+                        <div class="wrap">
+                            <div class="title">Best Moments</div>
+                            <div class="slider-content-1">
+                                <h4>ini slider 1</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
-        <div id="content">
-            <h2>Content</h2>
-            <p>This is the main content section.</p>
-        </div>
-
     </div>
 </section>
