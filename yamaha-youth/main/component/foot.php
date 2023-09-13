3a0d266466d1d8145edf92c9230e9f57203f08e6
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2023-09-13 14:58:24 +0700

    yamaha

--- yamaha-youth/main/component/foot.php
+++ yamaha-youth/main/component/foot.php
@@ -6,7 +6,7 @@
   <script src="./assets/js/index.js" type="module"></script>
   <script src="./node_modules/aos/dist/aos.js"></script>
   <script src="./assets/js/photo-gallery.js"></script>
-  <script src="./assets/js/sidebar.js"></script>
+  <!-- <script src="./assets/js/sidebar.js"></script> -->
   <script>
     // Initialize tooltips
     var tooltipTriggerList = [].slice.call(
@@ -15,4 +15,29 @@
     var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
       return new bootstrap.Tooltip(tooltipTriggerEl);
     });
+  </script>
+
+  <script>
+    $(function() {
+    var top = $('#destination').offset().top - parseFloat($('#destination').css('marginTop').replace(/auto/, 0));
+    var footTop = $('#bottom').offset().top - parseFloat($('#bottom').css('marginTop').replace(/auto/, 0));
+
+    var maxY = footTop - $('#destination').outerHeight();
+
+    $(window).scroll(function(evt) {
+      var y = $(this).scrollTop();
+      if (y > top) {
+        if (y < maxY) {
+          $('#destination').addClass('fixed').removeAttr('style');
+        } else {
+          $('#destination').removeClass('fixed').css({
+            position: 'absolute',
+            top: (maxY - top) + 'px'
+          });
+        }
+      } else {
+        $('#destination').removeClass('fixed');
+      }
+    });
+  });
   </script>
