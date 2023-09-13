6b2888293e8f66c4ddf03427c6732d2d857607d2
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2023-09-13 17:34:43 +0700

    yamaha

--- yamaha-youth/main/component-schoolcompetition/content.php
+++ yamaha-youth/main/component-schoolcompetition/content.php
@@ -5,52 +5,54 @@
                 <div class="col-12 col-md-4">
                     <div class="left-side" id="left-side">
                         <div class="destination" id="destination">
-                            <div class="latarkota">
-                                <img class="" src="./assets/images/school-competition/Background Kota.png" alt="">
+                            <div class="wrap-destination">
+                                <div class="latarkota">
+                                    <img class="" src="./assets/images/school-competition/Background Kota.png" alt="">
 
-                                <div class="wrap-desc mx-auto d-block">
-                                    <div class="logo-wrap">
-                                        <img class="img-fluid mx-auto d-block" src="./assets/images/school-competition/img-school.png" alt="">
-                                    </div>
-                                    <div class="about-destination text-center">
-                                        <div class="title">
-                                            <h4>SCHOOL COMPETITION</h4>
-                                            <h4>BANDUNG</h4>
-                                            <div class="captions">
-                                                <p>
-                                                    School Competition Classy Yamaha Youth Festival kota Bandung membawa siswa-siswi SMA terpilih untuk bertanding di [Nama GOR]. Menghadirkan berbagai kompetisi menarik seperti basket, futsal, cheerleading, dan Kompetisi OOTD (Outfit of The Day).
-                                                </p>
+                                    <div class="wrap-desc mx-auto d-block">
+                                        <div class="logo-wrap">
+                                            <img class="img-fluid mx-auto d-block" src="./assets/images/school-competition/img-school.png" alt="">
+                                        </div>
+                                        <div class="about-destination text-center">
+                                            <div class="title">
+                                                <h4>SCHOOL COMPETITION</h4>
+                                                <h4>BANDUNG</h4>
+                                                <div class="captions">
+                                                    <p>
+                                                        School Competition Classy Yamaha Youth Festival kota Bandung membawa siswa-siswi SMA terpilih untuk bertanding di [Nama GOR]. Menghadirkan berbagai kompetisi menarik seperti basket, futsal, cheerleading, dan Kompetisi OOTD (Outfit of The Day).
+                                                    </p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
 
-                                    <div class="share">
+                                        <div class="share">
 
-                                    </div>
-                                    <div class="wrap-kategori">
-                                        <div class="kategori">
-                                            <span>Best Moments</span>
                                         </div>
-                                        <div class="kategori">
-                                            <span>Miss Classy</span>
-                                        </div>
-                                        <div class="kategori">
-                                            <span>Outfit Of The Day</span>
-                                        </div>
-                                        <div class="kategori">
-                                            <span>Futsal</span>
-                                        </div>
-                                        <div class="kategori">
-                                            <span>Basketball</span>
-                                        </div>
-                                        <div class="kategori">
-                                            <span>Supporter</span>
-                                        </div>
-                                        <div class="kategori">
-                                            <span>Cheersleader / Modern Dance</span>
-                                        </div>
-                                        <div class="kategori">
-                                            <span>Content Creation challenge</span>
+                                        <div class="wrap-kategori">
+                                            <div class="kategori">
+                                                <span>Best Moments</span>
+                                            </div>
+                                            <div class="kategori">
+                                                <span>Miss Classy</span>
+                                            </div>
+                                            <div class="kategori">
+                                                <span>Outfit Of The Day</span>
+                                            </div>
+                                            <div class="kategori">
+                                                <span>Futsal</span>
+                                            </div>
+                                            <div class="kategori">
+                                                <span>Basketball</span>
+                                            </div>
+                                            <div class="kategori">
+                                                <span>Supporter</span>
+                                            </div>
+                                            <div class="kategori">
+                                                <span>Cheersleader / Modern Dance</span>
+                                            </div>
+                                            <div class="kategori">
+                                                <span>Content Creation challenge</span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
