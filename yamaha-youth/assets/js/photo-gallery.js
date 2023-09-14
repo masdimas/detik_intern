06a3723add0e1f19d8461e25cccbc5286db1f4bb
Arraidh <ari.darmika@gmail.com>
2023-09-14 17:48:50 +0700

    Style: Update Welcome Page Photo Gallery

--- yamaha-youth/assets/js/photo-gallery.js
+++ yamaha-youth/assets/js/photo-gallery.js
@@ -1,6 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
+  loop: true,
   spaceBetween: 10,
   slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+
   breakpoints: {
     // when window width is >= 320px
     280: {
@@ -16,11 +20,20 @@ var swiper = new Swiper(".mySwiper", {
       slidesPerView: 5,
       spaceBetween: 10,
     },
+    997: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
   },
   freeMode: true,
   watchSlidesProgress: true, // Navigation arrows
 });
 var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true,
+  },
   spaceBetween: 10,
   navigation: {
     nextEl: ".pg-arrow-next",
