f0b0ab93cd98c4eab37c1c2acc27cb00d8500038
Arraidh <ari.darmika@gmail.com>
2023-09-15 17:06:57 +0700

    Style: Update Miss Classy Page

--- yamaha-youth/main/component-miss-classy/miss-classy-vote-card.php
+++ yamaha-youth/main/component-miss-classy/miss-classy-vote-card.php
@@ -1,44 +1,6 @@
 
-<!-- Default State -->
+<!-- Voted and Not logged in State -->
 <div class="row  gap-sm-3 gap-2 w-100">
-        
-            <div class="vote-miss-classy-card d-flex flex-column align-items-center col-6 ">
-                <div class="miss-classy-img-container mx-auto ">
-                    <img
-                    src="./assets/images/miss-classy/miss-classy-pic/miss-classy-1.jpg"
-                    alt=""
-                    />
-                </div>
-                <div class="w-100">
-                    <div class="d-flex flex-column align-items-center gap-1 mt-2">
-                        <h4 class="candidate-name">Charlotte Brown</h4>
-                        <span class="candidate-school">SMAN 1 Bandung</span>
-                    </div>
-                    <button class="vote-button rounded-pill mt-2">VOTE</button>
-                </div>
-            </div>
-            <div class="vote-miss-classy-card d-flex flex-column align-items-center col-6 ">
-                <div class="miss-classy-img-container mx-auto ">
-                    <img
-                    src="./assets/images/miss-classy/miss-classy-pic/miss-classy-1.jpg"
-                    alt=""
-                    />
-                </div>
-                <div class="w-100">
-                    <div class="d-flex flex-column align-items-center gap-1 mt-2">
-                        <h4 class="candidate-name">Charlotte Brown</h4>
-                        <span class="candidate-school">SMAN 1 Bandung</span>
-                    </div>
-                    <button class="vote-button rounded-pill mt-2">VOTE</button>
-                </div>
-            </div>
-                        
-</div>
-
-
-<!-- Voted State -->
-<div class="row  gap-sm-3 gap-2 w-100">
-        
             <div class="vote-miss-classy-card d-flex flex-column align-items-center col-6 ">
                 <div class="miss-classy-img-container mx-auto ">
                     <img
@@ -57,7 +19,6 @@
                     </button>
                 </div>
             </div>
-        
             <div class="vote-miss-classy-card d-flex flex-column align-items-center col-6 ">
                 <div class="miss-classy-img-container mx-auto ">
                     <img
@@ -67,54 +28,14 @@
                 </div>
                 <div class="w-100">
                     <div class="d-flex flex-column align-items-center gap-1 mt-2">
-                        <h4 class="candidate-name">Charlotte Brown</h4>
-                        <span class="candidate-school">SMAN 1 Bandung</span>
-                    </div>
-                    <button class="vote-button rounded-pill mt-2 d-flex align-items-center justify-content-center gap-2 voted">
-                        <img src="./assets/images/miss-classy/miss-classy-pic/voted-icon.svg" alt="V">
-                        VOTED
-                    </button>
-                </div>
-            </div>
-     
-                        
-</div>
-
-<!-- Not Login State -->
-<div class="row  gap-sm-3 gap-2 w-100">
-        
-            <div class="vote-miss-classy-card d-flex flex-column align-items-center col-6 ">
-                <div class="miss-classy-img-container mx-auto ">
-                    <img
-                    src="./assets/images/miss-classy/miss-classy-pic/miss-classy-1.jpg"
-                    alt=""
-                    />
-                </div>
-                <div class="w-100">
-                    <div class="d-flex flex-column align-items-center gap-1 mt-2">
-                        <h4 class="candidate-name">Charlotte Brown</h4>
-                        <span class="candidate-school">SMAN 1 Bandung</span>
-                    </div>
-                    <button class="vote-button rounded-pill mt-2">VOTE</button>
-                </div>
-            </div>
-            <div class="vote-miss-classy-card d-flex flex-column align-items-center col-6 ">
-                <div class="miss-classy-img-container mx-auto ">
-                    <img
-                    src="./assets/images/miss-classy/miss-classy-pic/miss-classy-1.jpg"
-                    alt=""
-                    />
-                </div>
-                <div class="w-100">
-                    <div class="d-flex flex-column align-items-center gap-1 mt-2">
-                        <h4 class="candidate-name">Ini Pake Modal Mazzeh</h4>
+                        <h4 class="candidate-name">Ini sudah vote Mazzeh</h4>
                         <span class="candidate-school">SMAN 2 Bandung</span>
                     </div>
-                    <button class="vote-button rounded-pill mt-2" data-bs-toggle="modal" data-bs-target="#exampleModal">VOTE</button>
+                    <button class="vote-button rounded-pill mt-2" data-bs-toggle="modal" data-bs-target="#notLoggedIn">VOTE</button>
 
 
                     <!-- Modal -->
-                    <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal fade " id="notLoggedIn" tabindex="-1" aria-labelledby="notLoggedInLabel" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered modal-lg">
                         <div class="modal-content p-3">
                             <div class="modal-header">
@@ -123,6 +44,7 @@
                             </div>
                             <div class="modal-body d-flex flex-column justify-content-center align-items-center">
                                 <h3>Masuk dulu yuk!</h3>
+                                
                                 <img src="./assets/images/miss-classy/miss-classy-pic/skuy-login-logo.svg" alt="" />
                                 <div class="modal-text-content d-flex flex-column align-items-center">
                                     <h5>Kamu bisa memberi vote setelah masuk</h5>
@@ -138,5 +60,150 @@
                     </div>
                 </div>
             </div>
+        
+
                         
 </div>
+
+
+<!-- Confirm-Success State -->
+<div class="row  gap-sm-3 gap-2 w-100">
+
+            <div class="vote-miss-classy-card d-flex flex-column align-items-center col-6 ">
+                <div class="miss-classy-img-container mx-auto ">
+                    <img
+                    src="./assets/images/miss-classy/miss-classy-pic/miss-classy-1.jpg"
+                    alt=""
+                    />
+                </div>
+                <div class="w-100">
+                    <div class="d-flex flex-column align-items-center gap-1 mt-2">
+                        <h4 class="candidate-name">Konfirmasi Vote</h4>
+                        <span class="candidate-school">SMAN 2 Bandung</span>
+                    </div>
+                    <button class="vote-button rounded-pill mt-2" data-bs-toggle="modal" data-bs-target="#exampleModal">VOTE</button>
+
+
+                    <!-- Modal -->
+                    <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-lg">
+                        <div class="modal-content p-3">
+                            <div class="modal-header">
+                                <!-- <h5 class="modal-title" id="exampleModalLabel">Modal title</h5> -->
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body d-flex flex-column justify-content-center align-items-center">
+                                <h3>Dukung Sekolahmu    </h3>
+                                
+                                <img src="./assets/images/miss-classy/miss-classy-pic/vote-confirmation.svg" alt="">
+                                <div class="modal-text-content d-flex flex-column align-items-center">
+                                    <h5>Yakin dengan Miss Classy Pilihanmu?</h5>
+                                    <span>Kamu punya kuota 1x vote setiap 30 menit.</span>
+
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button class="mx-auto modal-cta rounded-pill">Ya, Vote Sekarang</button>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div> 
+            
+            <div class="vote-miss-classy-card d-flex flex-column align-items-center col-6 ">
+                <div class="miss-classy-img-container mx-auto ">
+                    <img
+                    src="./assets/images/miss-classy/miss-classy-pic/miss-classy-1.jpg"
+                    alt=""
+                    />
+                </div>
+                <div class="w-100">
+                    <div class="d-flex flex-column align-items-center gap-1 mt-2">
+                        <h4 class="candidate-name">Charlotte Brown</h4>
+                        <span class="candidate-school">SMAN 1 Bandung</span>
+                    </div>
+                    <button class="vote-button rounded-pill mt-2">VOTE</button>
+                </div>
+            </div>
+</div>
+
+<!-- Not Login State -->
+
+
+<!-- Vote Confirmation State -->
+<div class="row  gap-sm-3 gap-2 w-100">
+
+
+
+            <div class="vote-miss-classy-card d-flex flex-column align-items-center col-6 ">
+                <div class="miss-classy-img-container mx-auto ">
+                    <img
+                    src="./assets/images/miss-classy/miss-classy-pic/miss-classy-1.jpg"
+                    alt=""
+                    />
+                </div>
+                <div class="w-100">
+                    <div class="d-flex flex-column align-items-center gap-1 mt-2">
+                        <h4 class="candidate-name">Charlotte Brown</h4>
+                        <span class="candidate-school">SMAN 1 Bandung</span>
+                    </div>
+                    <button class="vote-button rounded-pill mt-2">VOTE</button>
+                </div>
+            </div>
+
+            <div class="vote-miss-classy-card d-flex flex-column align-items-center col-6 ">
+                <div class="miss-classy-img-container mx-auto ">
+                    <img
+                    src="./assets/images/miss-classy/miss-classy-pic/miss-classy-1.jpg"
+                    alt=""
+                    />
+                </div>
+                <div class="w-100">
+                    <div class="d-flex flex-column align-items-center gap-1 mt-2">
+                        <h4 class="candidate-name">Charlotte Brown</h4>
+                        <span class="candidate-school">SMAN 1 Bandung</span>
+                    </div>
+                    <button class="vote-button rounded-pill mt-2">VOTE</button>
+                </div>
+            </div>
+
+                        
+</div>
+
+<!-- After Vote State -->
+<div class="row  gap-sm-3 gap-2 w-100">
+
+            <div class="vote-miss-classy-card d-flex flex-column align-items-center col-6 ">
+                <div class="miss-classy-img-container mx-auto ">
+                    <img
+                    src="./assets/images/miss-classy/miss-classy-pic/miss-classy-1.jpg"
+                    alt=""
+                    />
+                </div>
+                <div class="w-100">
+                    <div class="d-flex flex-column align-items-center gap-1 mt-2">
+                        <h4 class="candidate-name">Charlotte Brown</h4>
+                        <span class="candidate-school">SMAN 1 Bandung</span>
+                    </div>
+                    <button class="vote-button rounded-pill mt-2">VOTE</button>
+                </div>
+            </div>
+            <div class="vote-miss-classy-card d-flex flex-column align-items-center col-6 ">
+                <div class="miss-classy-img-container mx-auto ">
+                    <img
+                    src="./assets/images/miss-classy/miss-classy-pic/miss-classy-1.jpg"
+                    alt=""
+                    />
+                </div>
+                <div class="w-100">
+                    <div class="d-flex flex-column align-items-center gap-1 mt-2">
+                        <h4 class="candidate-name">Charlotte Brown</h4>
+                        <span class="candidate-school">SMAN 1 Bandung</span>
+                    </div>
+                    <button class="vote-button rounded-pill mt-2">VOTE</button>
+                </div>
+            </div>
+                        
+</div>
+
