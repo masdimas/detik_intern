e7142c4f42f8b8e5b766e7ffa724c3fbb99372a4
Dimas <dimas.septiandi@detik.com>
2023-09-19 17:32:59 +0700

    Update LPS

--- festivalcreative-lps/participation-gallery.php
+++ festivalcreative-lps/participation-gallery.php
@@ -5,12 +5,18 @@
 <?php include "section/home/navbar.php"; ?>
     <?php include "section/home/headline.php"; ?>
     <div class="mb-5"></div>
-    <?php include "section/home/banner.php"; ?>
+    <?php 
+        if($_GET["year"] == "2023"){
+            include "section/home/banner.php";
+        }
+    ?>
     <?php
         if($_GET["gallery"] == "video"){
             include "section/home/participation-gallery-video.php";
         } else if($_GET["gallery"] == "poster"){
             include "section/home/participation-gallery-poster.php";
+        } else if($_GET["gallery"] == "video-2022"){
+            include "section/home/participation-gallery-video-2022.php";
         }
     ?>
     <?php include "section/home/footer.php"; ?>
