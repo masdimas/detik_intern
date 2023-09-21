86e6c6096056b1ce2a829be51e5d0bcbcaca36bb
Dimas <dimas.septiandi@detik.com>
2023-09-21 15:31:05 +0700

    Update Hero Yamaha

--- yamaha-youth/main/component/hero.php
+++ yamaha-youth/main/component/hero.php
@@ -3,11 +3,14 @@
       id="home"
     >
       <div
-        class="container d-flex flex-column align-items-center justify-content-center gap-4 hero-content"
+        class="container d-flex flex-column align-items-center justify-content-center gap-3 hero-content"
       >
         <img  src="./assets/images/hero/hero-logo.svg" alt="" class="w-lg-100 w-75" />
         <div class="hero-desc gill-sans-1000 text-center">
-          DUKUNG SEKOLAHMU DENGAN PENUH GAYA!
+         Festival kompetisi antar sekolah ter-classy tahun ini
+        </div>
+        <div class="sub-desc gill-sans-1000 text-center d-none d-sm-block">
+         yuk dukung sekolah favoritmu sekarang!
         </div>
       </div>
       <div
