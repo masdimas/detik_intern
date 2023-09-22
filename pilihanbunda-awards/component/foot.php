03acc3f53583ff2dbc7e9b10ed139e1a00894041
Kadek Ari Dharmika <ari.darmika@gmail.com>
2023-09-22 12:36:10 +0700

    Style: Add Voting Page

--- pilihanbunda-awards/component/foot.php
+++ pilihanbunda-awards/component/foot.php
@@ -1,5 +1,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.1/js/bootstrap.bundle.min.js"></script>
 
+<!-- Old Navbar Script -->
 <script>
     myID = document.getElementById("myID");
 
@@ -22,5 +23,26 @@
         $('ul.navbar-nav a').filter(function() {
             return this.href == url;
         }).addClass('active');
+    });
+</script>
+
+<!-- Script for copy link tooltip popup -->
+<script>
+    $(document).ready(function() {
+      // Initialize the tooltip
+      $('#copy-link').tooltip();
+
+      // Add a click event to show the tooltip
+      $('#copy-link').on('click', function() {
+        var tooltip = $(this);
+
+        // Show the tooltip
+        tooltip.tooltip('show');
+
+        // Add a timeout to hide the tooltip after 3 seconds
+        setTimeout(function() {
+          tooltip.tooltip('hide');
+        }, 3000); // 3000 milliseconds (3 seconds)
+      });
     });
 </script>
--- pilihanbunda-awards/component/voting-content.php
+++ pilihanbunda-awards/component/voting-content.php
@@ -1,12 +1,72 @@
 <div id="votingform">
     <div class="container mb-5">
         <div class="header-voting mb-5">
-            <h1>SURVEI PILIHAN BUNDA</h1>
-            <p>Survei Pilihan Bunda telah berakhir. Sebagai bentuk rasa terima kasih kami, untuk Bunda yang beruntung akan mendapatkan hadiah berupa saldo e-wallet senilai Rp10 juta untuk 100 Bunda. Pemenang akan ditampilkan diumumkan di Artikel HaiBunda Pilihan Bunda Awards 2023.<br><br>Periode: 18 Agustus 2023 - 18 September 2023
+            <h1>VOTING PILIHAN BUNDA</h1>
+            <p>Ikutan voting "Pilihan Bunda" yuk, Bun! Rekomendasikan produk terbaik untuk keperluan sehari-hari untuk Bunda dan keluarga. Ada saldo e-wallet senilai total Rp10 juta rupiah untuk 100 Bunda yang beruntung.<br><br>Periode: 14 Oktober - 1 Desember 2023
             </p>
+
+            <div class="d-flex justify-content-center " id="sosmed">
+                <div class="social-media-share rounded-pill mt-2" id="social-media-share">
+                    <span>Share</span>
+                    <img src="./src/image/voting/facebook.svg" alt="">
+                    <img src="./src/image/voting/twitter.svg" alt="">
+                    <img src="./src/image/voting/whatsapp.svg" alt="">
+                    <img src="./src/image/voting/telegram.svg" alt="">
+
+
+                    <div id="copy-link" data-bs-trigger="manual" data-bs-placement="bottom" data-bs-animation="true" data-bs-toggle="tooltip" title="Tautan Disalin">
+                        <img src="./src/image/voting/copylink.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="voting-instruction mb-5">
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Cara Voting
+                    </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                        Tata cara voting dengan polong:
+                            <ol class="rounded">
+                                <li>Masuk menggunakan akun MPC atau daftar menggunakan email / nomor HP.</li>
+                                <li>Pilih salah satu Brand/Tokoh pilihan Bunda untuk masing-masing kategori yang Bunda rekomendasikan.</li>
+                                <li>Apabila ada kategori yang <b>tidak sesuai</b> dengan kondisi atau pengalaman bunda pilih “tidak memilih kategori”</li>
+                                <li>Klik “Submit” jika sudah yakin dengan pilihan Bunda</li>
+                            </ol>   
+                            Tata cara voting dengan Google:
+                            <ol>
+                                <li>Masuk menggunakan akun google atau gmail Bunda.</li>
+                                <li>Pilih salah satu Brand/Tokoh pilihan Bunda untuk masing-masing kategori yang Bunda rekomendasikan.</li>
+                                <li>Apabila ada kategori yang <b>tidak sesuai</b> dengan kondisi atau pengalaman bunda pilih “tidak memilih kategori”</li>
+                                <li>Klik “Submit” jika sudah yakin dengan pilihan Bunda</li>
+                            </ol> 
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Cara Voting Menggunakan Google
+                    </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                              
+                        </div>
+                    </div>
+                </div> -->
+
+            </div>
         </div>
         <div class="embed-voting">
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdu_jspRPPOZ7SdbCv9K_xWHcBhraddptYCZjQaKmra7kcP_Q/viewform?embedded=true" width="640" height="1118" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </div>
+     
     </div>
+   
 </div>
