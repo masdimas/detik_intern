b28f86710cbfa0fb20f32ff3e008a774f87feea9
Dimas <dimas.septiandi@detik.com>
2023-09-26 15:52:31 +0700

    Update JS SC

--- yamaha-youth/main/component/foot.php
+++ yamaha-youth/main/component/foot.php
@@ -60,35 +60,7 @@
     });
   </script>
 
-  <script>
-    $(function() {
-      var top = $('#wrap-kategori').offset().top - parseFloat($('#wrap-kategori').css('marginTop').replace(/auto/, 0));
-      var footTop = $('#footer').offset().top - parseFloat($('#footer').css('marginTop').replace(/auto/, 0));
-
-      var maxY = footTop - $('#wrap-kategori').outerHeight();
-
-      $(window).scroll(function(evt) {
-        var wi = $(window).width();
-
-        var y = $(this).scrollTop();
-
-        if (wi >= 767) {
-          if (y > top) {
-            if (y < maxY) {
-              $('#wrap-kategori').addClass('fixed').removeAttr('style');
-            } else {
-              $('#wrap-kategori').removeClass('fixed').css({
-                position: 'absolute',
-                top: (maxY - top) + 'px'
-              });
-            }
-          } else {
-            $('#wrap-kategori').removeClass('fixed');
-          }
-        }
-      });
-    });
-  </script>
+  
 
   <script>
     $(function() {
--- yamaha-youth/schoolcompetition.php
+++ yamaha-youth/schoolcompetition.php
@@ -26,6 +26,35 @@ include "./main/component/head.php";
 include "./main/component/foot.php";
 ?>
 
+<script>
+    $(function() {
+      var top = $('#wrap-kategori').offset().top - parseFloat($('#wrap-kategori').css('marginTop').replace(/auto/, 0));
+      var footTop = $('#footer').offset().top - parseFloat($('#footer').css('marginTop').replace(/auto/, 0));
+
+      var maxY = footTop - $('#wrap-kategori').outerHeight();
+
+      $(window).scroll(function(evt) {
+        var wi = $(window).width();
+
+        var y = $(this).scrollTop();
+
+        if (wi >= 767) {
+          if (y > top) {
+            if (y < maxY) {
+              $('#wrap-kategori').addClass('fixed').removeAttr('style');
+            } else {
+              $('#wrap-kategori').removeClass('fixed').css({
+                position: 'absolute',
+                top: (maxY - top) + 'px'
+              });
+            }
+          } else {
+            $('#wrap-kategori').removeClass('fixed');
+          }
+        }
+      });
+    });
+  </script>
 
 <!-- <script type="text/javascript" src="assets/js/countdown.js"></script> -->
 
