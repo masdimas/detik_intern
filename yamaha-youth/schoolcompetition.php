6f12b3b0d31160a74d5cf183fa14a66713295ad5
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2023-10-02 13:52:03 +0700

    Update schoolcompetition.php

--- yamaha-youth/schoolcompetition.php
+++ yamaha-youth/schoolcompetition.php
@@ -5,19 +5,19 @@ include "./main/component/head.php";
 ?>
 
 <body>
-    <!-- Navbar -->
-    <?php
-    include "./main/component/navbar.php";
-    ?>
+  <!-- Navbar -->
+  <?php
+  include "./main/component/navbar.php";
+  ?>
 
-<?php
-    include "./main/component-schoolcompetition/content.php";
-    ?>
+  <?php
+  include "./main/component-schoolcompetition/content.php";
+  ?>
 
-    <!-- Footer -->
-    <?php
-    include "./main/component/footer.php";
-    ?>
+  <!-- Footer -->
+  <?php
+  include "./main/component/footer.php";
+  ?>
 
 </body>
 
@@ -27,34 +27,35 @@ include "./main/component/foot.php";
 ?>
 
 <script>
-    $(function() {
-      var top = $('#wrap-kategori').offset().top - parseFloat($('#wrap-kategori').css('marginTop').replace(/auto/, 0));
-      var footTop = $('#footer').offset().top - parseFloat($('#footer').css('marginTop').replace(/auto/, 0));
+  $(function() {
+    var top = $('#wrap-kategori').offset().top - parseFloat($('#wrap-kategori').css('marginTop').replace(/auto/, 0));
+    var footTop = $('#footer').offset().top - parseFloat($('#footer').css('marginTop').replace(/auto/, 0));
 
-      var maxY = footTop - $('#wrap-kategori').outerHeight();
+    var maxY = footTop - $('#wrap-kategori').outerHeight();
 
-      $(window).scroll(function(evt) {
-        var wi = $(window).width();
+    $(window).scroll(function(evt) {
+      var wi = $(window).width();
 
-        var y = $(this).scrollTop();
+      var y = $(this).scrollTop();
 
-        if (wi >= 767) {
-          if (y > top) {
-            if (y < maxY) {
-              $('#wrap-kategori').addClass('fixed').removeAttr('style');
-            } else {
-              $('#wrap-kategori').removeClass('fixed').css({
-                position: 'absolute',
-                top: (maxY - top) + 'px'
-              });
-            }
+      if (wi >= 767) {
+        if (y >= top - $('#social-media-share').height()) {
+          if (y < maxY) {
+            $('#wrap-kategori').addClass('fixed').removeAttr('style');
           } else {
-            $('#wrap-kategori').removeClass('fixed');
+            $('#wrap-kategori').removeClass('fixed').css({
+              position: 'absolute',
+              // top: (maxY - top) + 'px'
+              top : (top + 60) + 'px',
+            });
           }
+        } else {
+          $('#wrap-kategori').removeClass('fixed').removeAttr('style');
         }
-      });
+      }
     });
-  </script>
+  });
+</script>
 
 <!-- <script type="text/javascript" src="assets/js/countdown.js"></script> -->
 
